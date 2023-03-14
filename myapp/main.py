7847a8bb5e262c89728bcbe382a55f419add26ce
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-03-14 19:51:54 -0400

    Calculator app

--- myapp/main.py
+++ myapp/main.py
@@ -1,7 +1,7 @@
-import kivy
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
-import random
+import kivy
 
 
 class MyRoot(BoxLayout):
@@ -9,15 +9,21 @@ class MyRoot(BoxLayout):
     def __init__(self):
         super(MyRoot, self).__init__()
 
-    def generate_number(self):
-        self.random_label.text = str(random.randint(0, 1000))
+    def calc_symbol(self, symbol):
+        self.calc_field.text += symbol
+
+    def clear(self):
+        self.calc_field.text = ""
+
+    def get_result(self):
+        self.calc_field.text = str(eval(self.calc_field.text))
 
 
-class MyApp(App):
+class Calc(App):
 
     def build(self):
         return MyRoot()
 
 
-myApp = MyApp()
-myApp.run()
+calc = Calc()
+calc.run()
